bf0c5a81b5da526a17518392ae6baa0f7f05429b
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-08-23 20:24:57 -0600

    Random Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Accordion from './components/1_Accordion';
+// import Accordion from './components/1_Accordion';
+import RandomColor from './components/2_Random-Color/Random.Color';
 
 export default function App() {
 
   return (
-    <Accordion />
+    <RandomColor />
   )
 }
 
