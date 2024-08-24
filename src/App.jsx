cba3cc0f0a9a4ed890e78aee6978cb10e519571c
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-08-23 19:47:39 -0600

    Initializing the project & adding the first project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Accordion from './components/1_Accordion';
+
+export default function App() {
+
+  return (
+    <Accordion />
+  )
+}
+
+
