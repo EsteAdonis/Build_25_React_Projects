ba62dad8f4a91380a5194b727de3f61a11930f21
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-08-24 18:42:36 -0600

    Star Rating

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 // import Accordion from './components/1_Accordion';
-import RandomColor from './components/2_Random-Color/Random.Color';
+// import RandomColor from './components/2_Random-Color/Random.Color';
+import StarRating from './components/3_Star-Rating/Star.Rating';
 
 export default function App() {
 
   return (
-    <RandomColor />
+    <StarRating />
   )
 }
 
