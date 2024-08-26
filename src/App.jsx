13c7e51a2eddecae2b6faa3f2bf8951fc0a4dcc7
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-08-25 19:45:16 -0600

    Image Slider

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@
 import './App.css'
 // import Accordion from './components/1_Accordion';
 // import RandomColor from './components/2_Random-Color/Random.Color';
-import StarRating from './components/3_Star-Rating/Star.Rating';
+// import StarRating from './components/3_Star-Rating/Star.Rating';
+import ImageSlider from './components/4_Image-Slider/Image.Slider';
 
 export default function App() {
 
   return (
-    <StarRating />
+    <ImageSlider
+      url={"https://picsum.photos/v2/list"}
+      page={"1"}
+      limit={"10"} />
   )
 }
 
